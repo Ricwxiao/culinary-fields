2ec58b96be1936c42f4ae293f0fd78ec3ad5c220
taigah-2039002 <taigah@uw.edu>
2024-02-27 11:47:43 -0800

    css and home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,13 +4,11 @@ import React from 'react';
 export default function HomePage(props) {
 
     return (
-        <div className = > 
-            <header>
-
-            </header>
-            <main>
-                
-            </main>
-        </div>
+        <div className = "topnav"> 
+            <a href= "#Home"> Home </a>
+            <a href= "#Farm&Food"> Farm&Food Finder</a>
+            <a href= "#Event"> Event </a>
+            <a href= "#Receipe"> Receipe </a>
+        </div> 
     )
 }
