c4c6eb62b3ae6a64eab11bf2f2b8f3e133e73320
taigah-2039002 <taigah@uw.edu>
2024-02-26 23:40:01 -0800

    small changes

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,1 +1,16 @@
 
+import React from 'react';
+
+export default function HomePage(props) {
+
+    return (
+        <div className = > 
+            <header>
+
+            </header>
+            <main>
+                
+            </main>
+        </div>
+    )
+}
