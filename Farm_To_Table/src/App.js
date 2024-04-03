277555ec17026d266021e27f34081f76069dff7b
Robert Bonglamphone <bonglrob@uw.edu>
2024-04-02 18:25:53 -0700

    add navbar components

--- Farm_To_Table/src/App.js
+++ Farm_To_Table/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
+// import component pages
+import Navbar from "./components/NavBar.js";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Routes>
+      <Route path="/" element={<Navbar/>}>
+          <Route path="/farmfinder" element={<farmfinder/>} />
+      </Route>
+    </Routes>
+  </div>
   );
 }
-
-export default App;
--- Farm_To_Table/src/components/NavBar.js
+++ Farm_To_Table/src/components/NavBar.js
@@ -1,4 +1,27 @@
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
+
 function Navbar() {
   return (
+    <div>
+      <nav>
+        <div>
+          <ul>
+            <li className='nav-item'>
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/farmfinder" className="nav-link">
+                Farm & Food Finder
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   )
 }
+
+export default Navbar;
