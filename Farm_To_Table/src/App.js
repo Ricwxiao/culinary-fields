48637939dc13438a7ec34ccc46b3a94eb6373e60
Robert Bonglamphone <bonglrob@uw.edu>
2024-04-02 18:42:19 -0700

    added navlink

--- Farm_To_Table/src/App.js
+++ Farm_To_Table/src/App.js
@@ -4,16 +4,20 @@ import { BrowserRouter as Router, Route, Routes, Navigate, Switch } from "react-
 import './App.css';
 
 // import component pages
-import Navbar from "./components/NavBar.js";
+// import Home from "./components/Home";
+import Navbar from "./components/NavBar";
 
 export default function App() {
   return (
     <div>
     <Routes>
       <Route path="/" element={<Navbar/>}>
+          {/* <Route path="/Home" element={<Home/>} /> */}
           <Route path="/farmfinder" element={<farmfinder/>} />
       </Route>
     </Routes>
   </div>
   );
 }
+
+// uncaught runtime errors potential solution: https://stackoverflow.com/questions/70423894/error-useroutes-may-be-used-only-in-the-context-of-a-router-component-even
