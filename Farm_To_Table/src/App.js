f47fa20e123719548f375217ef9058f866b4d685
ryanvle <ryanvle@uw.edu>
2024-04-12 13:44:22 -0700

    stashing before switch

--- Farm_To_Table/src/App.js
+++ Farm_To_Table/src/App.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
-=======
 import React, { useState } from 'react';
 import { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, Switch } from "react-router-dom";
 import FarmerInfo from "./components/FarmerInfo/FarmerInfo";
 import Recipe from "./components/Recipe";
->>>>>>> origin
+import Footer from "./components/Footer";
 
 // import component pages
 // import Home from "./components/Home";
@@ -13,31 +11,9 @@ import Navbar from "./components/NavBar";
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <h1>
-            Hello World
-          </h1>
-
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-=======
     <div>
       <header>
         <Navbar/>
->>>>>>> origin
       </header>
       <Routes>
         <Route path="/" element={<Recipe />} />
