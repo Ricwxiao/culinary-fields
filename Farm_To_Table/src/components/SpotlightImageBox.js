108bb0ddd038abda6885881fc617b99aea9a323e
taigah <taigah@uw.edu>
2024-05-02 10:19:04 -0700

    taiga changes

---
+++ Farm_To_Table/src/components/SpotlightImageBox.js
@@ -0,0 +1,21 @@
+
+// import "./ImageBox.css"
+import "../index.css"
+import ImageIcon from "./ImageIcon"
+
+export default function SpotlightImageBox ({src, text, text2, alt, width, height}) {
+  return (
+    <div className="image-container">
+      {/* <div class="Overlay">
+
+      </div> */}
+      <img src={src} alt={alt} width={width} height={height} />
+      <div className="Overlay">
+        <p className="image-icon">{text2}</p>
+        <p className="text-overlay">{text}</p>
+
+      </div>
+    </div>
+  )
+}
+
