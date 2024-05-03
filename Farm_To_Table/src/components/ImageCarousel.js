3d729a49374e0a51d7ad0ebdc62d1070106bb746
taigah <taigah@uw.edu>
2024-05-03 16:30:57 -0700

    transparent background

--- Farm_To_Table/src/components/ImageCarousel.js
+++ Farm_To_Table/src/components/ImageCarousel.js
@@ -11,22 +11,16 @@ function ImageCarousel({ height, width }) {
   return (
     <div className="image-carousel-container" style={{ height: height, width: width }}>
       <div className="left-panel">
-<<<<<<< HEAD
-        <NavLink to="/spotlight" className="nav-link">
-        {/* <img src={spotlightImg} alt="farmer spotlight" width="500px" height="500px"/> */}
-        <ImageBox src={spotlightImg} alt="farmer spotlight" width="500px" height="500px" text="Story by Tian Tian Farm: Farming for the Asian Community" />
-=======
         <NavLink to="/farmfinder" className="nav-link">
         <SpotlightImageBox src={spotlightImg} alt="farmer spotlight" width="100%" height="500px" text="Story by Tian Tian Farm: Farming for the Asian Community" text2="Most Liked"/>
 
->>>>>>> homepage
         </NavLink>
 
       </div>
       <div className="right-panel">
         <div className="top-image">
           {/* <img src={bokchoyreceipeImg} alt="farmer spotlight" width="100%" height="100%"/> */}
-          <SpotlightImageBox src={bokchoyreceipeImg1} alt="farmer spotlight" width="" height="500px" text="Bok Choy in season: Here are some of the most popular recipes" text2="In Season" />
+          <SpotlightImageBox src={bokchoyreceipeImg1} alt="farmer spotlight" width="600px" height="500px" text="Bok Choy in season: Here are some of the most popular recipes" text2="In Season" />
 
         </div>
         {/* <div className="bottom-image">
