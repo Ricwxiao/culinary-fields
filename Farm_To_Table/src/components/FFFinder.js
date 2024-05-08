33253cc850ea07f03aa92d25cb7335959ab956ff
Ricwxiao <cxiao4@uw.edu>
2024-05-07 21:08:58 -0700

    added logo to fffinder list view

--- Farm_To_Table/src/components/FFFinder.js
+++ Farm_To_Table/src/components/FFFinder.js
@@ -20,13 +20,16 @@ export default function FFFinder(props) {
         return(
             farmsData.map((farmObj) => (
                     <div className="farmListItem" key={farmObj.id}>
-                        <h3>{farmObj.name}</h3>
-                        <a href={farmObj.website} className="websiteLink"><span class="material-symbols-outlined">language</span><span className="textInAnchor">Website</span></a>
-                        <p><span class="material-symbols-outlined">pin_drop</span>{farmObj.address}</p>
-                        <div className="learnMoreBtn">
-                            <NavLink to={`/farmerinfo/${farmObj.id}`}>
-                                <button>Learn More</button>
-                            </NavLink>
+                        <img src={farmObj.logo} className="farmLogo"/>
+                        <div className="textInfo">
+                            <h3>{farmObj.name}</h3>
+                            <a href={farmObj.website} target="_blank" className="websiteLink"><span class="material-symbols-outlined">language</span><span className="textInAnchor">Website</span></a>
+                            <p><span class="material-symbols-outlined">pin_drop</span>{farmObj.address}</p>
+                            <div className="learnMoreBtn">
+                                <NavLink to={`/farmerinfo/${farmObj.id}`}>
+                                    <button>Learn More</button>
+                                </NavLink>
+                            </div>
                         </div>
                     </div>
                 )
