5c867caacdcf09c02686e5798a0d4d9cd3a500d1
Robert Bonglamphone <rob.bonglamphone@gmail.com>
2024-05-09 17:50:09 -0700

    farmerbio

--- Farm_To_Table/src/components/FarmerInfo/FarmerInfo.js
+++ Farm_To_Table/src/components/FarmerInfo/FarmerInfo.js
@@ -99,14 +99,15 @@ export default function FarmerInfo(props) {
             <div className="stories">
                 <h2>Our Stories</h2>
                 <p>
+                    {farm.bio}
                     Elizabeth Whitman and Steven Hsieh began dreaming up Tian Tian Farm in 2020 during apprenticeships at organic farms in Oregon.
                 </p>
-                <p>
+                {/* <p>
                     Elizabeth is a second-generation Chinese American. She grew up eating home-cooked kungpao chicken (宮保雞丁), tofu stir fry (豆腐乾), and her grandfather’s hot chili oil (辣椒). Her farming aspirations date back to childhood, when she tried to plant corn, carrots, and cucumbers in the rock-stubbled clay soils of her home state of Connecticut. Before farming, she worked full-time as a journalist in Jordan, New York, and Arizona. Her cat, Scout, is Tian Tian’s director of rodent control.
                 </p>
                 <p>
                     Steven is a second-generation Taiwanese American whose fondest food memories revolve around Sunday morning dim sum. Also a former newspaper reporter, he decided to switch careers after discovering a new kind of joy during misty morning harvests. He lived in Seattle from 2017 to 2018, when he served as news editor at The Stranger.
-                </p>
+                </p> */}
             </div>
             <div className="starringContainer">
                 <h2>Starring Recipes</h2>
